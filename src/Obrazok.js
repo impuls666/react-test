c0b51217ffa2540f90252ca0bfc020fb0a08aea2
Impuls <impuls666@gmail.com>
2018-06-10 14:18:00 +0200

    upravy 10/06/2018

--- src/Obrazok.js
+++ src/Obrazok.js
@@ -6,7 +6,7 @@ export default class Obrazok extends React.Component {
       return(
       <div className="col-md-3">
       <img className='gallery-thumbnail' src={this.props.objekty} alt={this.props.alt} />
-      <a href={this.props.url} className='card-icon-open'></a>
+      <a className="card-icon-open" href={this.props.url}></a>
       </div>
       )
     }
--- src/Test.js
+++ src/Test.js
@@ -9,20 +9,22 @@ import Obrazok from './Obrazok';
 export default class Test extends React.Component {
     render()
     {
+      var pici =  OBJEKTY.filter((objekt)=> {         
+        return objekt.url === this.props.location.pathname;                           
+      })   
+      console.log(this.props.location.pathname);
       return(
         <div className="row">
-        <div className="container">     
-       {
-       OBJEKTY.map((objekt)=> {         
-           return objekt.subimages.map((odkaz, index)=> {             
-              return <Obrazok objekty={odkaz} alt={'Image number ' + (index + 1)} />
-            })
-          })     
-        } 
+        <div className="container">
+        {     
+               pici.map((objekt)=> {         
+                return objekt.subimages.map((odkaz, index)=> {             
+                   return <Obrazok objekty={odkaz} alt={'Image number ' + (index + 1)} />
+                 })
+               })  
+        }          
        </div> 
        </div>
       )
     }
 }
-
-
